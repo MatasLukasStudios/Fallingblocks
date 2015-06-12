67a7180adee0df165431f999afa0eff9c20e114b
Matas <dev.matas@gmail.com>
2015-06-12 15:52:06 +0300

    Pirmas ikelimas

---
+++ Assets/Scripts/ClickerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickerController : MonoBehaviour {
+
+	// Use this for initialization
+    Animator anim;
+    public static bool gameOver;
+
+	void Start () 
+    {
+        anim = this.gameObject.GetComponentInChildren<Animator>();
+        gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(gameOver)
+        {
+            
+            Time.timeScale = 0;
+        }
+        
+	
+	}
+
+    void OnMouseDown()
+    {
+        anim.SetTrigger("Flash");
+        GameObject[] blueBoxes = GameObject.FindGameObjectsWithTag("Blue");
+        GameObject[] orangeBoxes = GameObject.FindGameObjectsWithTag("Orange");
+        GameObject[] purpleBoxes = GameObject.FindGameObjectsWithTag("Purple");
+        for (int i = 0; i < blueBoxes.Length; i++)
+        {
+            if (this.gameObject.tag == blueBoxes[i].gameObject.tag + "Box" && blueBoxes[i].transform.position.y <= -3)
+            {
+                Spawner.score++;
+                Destroy(blueBoxes[i]);
+            }
+          
+        }
+        for(int b = 0;b < orangeBoxes.Length; b++)
+        {
+            if (this.gameObject.tag == orangeBoxes[b].gameObject.tag + "Box" && orangeBoxes[b].transform.position.y <= -3)
+            {
+                Spawner.score++;
+                Destroy(orangeBoxes[b]);
+            }
+            
+        }
+        for(int c = 0;c < orangeBoxes.Length; c++)
+        {
+            if (this.gameObject.tag == orangeBoxes[c].gameObject.tag + "Box" && orangeBoxes[c].transform.position.y <= -3)
+            {
+                Spawner.score++;
+                Destroy(orangeBoxes[c]);
+            }
+           
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] circles;
+    public GameObject[] spawnerPositions;
+    public static int level = 0;
+    public static int score;
+    private GameObject scoreText;
+    public GameObject mainCamera;
+    private Animator anim;
+	// Use this for initialization
+	void Start () 
+    {
+        InvokeRepeating("SpawnCircle", 1, 1);
+        scoreText = GameObject.Find("Score");
+        anim = mainCamera.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        scoreText.GetComponent<Text>().text = "Score: " + score;
+        if(score >=6)
+        {
+            level = 1;
+        }
+        if(level == 1)
+        {
+            anim.SetInteger("Hardness", level);
+        }
+
+	}
+
+    void SpawnCircle()
+    {
+        int ranSide0index = Random.Range(0,3);
+        int ranColor0index = Random.Range(0, 3);
+        GameObject circle = (GameObject)Instantiate(circles[ranColor0index]);
+        Vector3 assignPos = spawnerPositions[ranSide0index].transform.position;
+        circle.transform.position = assignPos;
+
+    }
+}
